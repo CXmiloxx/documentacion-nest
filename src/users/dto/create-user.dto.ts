b7e5f3a42843aeb6406b2cc88259fe1c11d88405
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-04-27 23:00:58 -0500

    ✨ Feat: se crean validaciones para mi tipo user en dto
    se instala class-validator y class-transformer para poder que nuestros tipos tengan validaciones del tipo de dato que requieren

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsString, IsNumber, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   age: number;
 }
