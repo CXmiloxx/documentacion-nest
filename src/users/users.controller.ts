1c324942e057f8f5237af601657d4ea8cff2f1f9
Camilo Guapacha <137653825+CXmiloxx@users.noreply.github.com>
2025-04-27 23:19:50 -0500

    ✨ Feat: agrega validacion de tipo de lo que viene del front
    esto se ha ce porque aunque declaremos que es de tipo number o demas no lo tomara como un numero si no que como un string como viene desde el front con esta validacion ahora si lo convertira en un number

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   HttpCode,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -45,7 +46,7 @@ export class UsersController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.remove(+id);
   }
 }
